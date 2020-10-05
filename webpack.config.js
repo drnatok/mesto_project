684283b55d0b49db3b28bf4068c4f769ad8cbb8e
mr.anderson <super.natok@yandex.ru>
2020-10-05 20:19:21 +0300

    fixed review advices

--- webpack.config.js
+++ webpack.config.js
@@ -9,56 +9,38 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
-  },
+    path: path.resolve(__dirname, 'dist'), filename: '[name].[chunkhash].js'},
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {loader: "babel-loader"}
       },
       {
-        test: /\.css$/i,
-        use: [
-          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-          'css-loader', 
-          'postcss-loader'
-            ]
+      test: /\.css$/i,
+      use: [(isDev ? 'style-loader': MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
       },
       {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=./vendor/[name].[ext]'
-    },
-    {
-        test: /\.(png|jpg|gif|ico|svg)$/,
-        use: [
-            'file-loader?name=./images/[name].[ext]', 
-            {
-                loader: 'image-webpack-loader',
-                options: {}
-            },
-            ]
- }
-
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]'
+      },
+      {
+      test: /\.(png|jpg|gif|ico|svg)$/,
+      use: ['file-loader?name=./images/[name].[ext]', {loader: 'image-webpack-loader', options: {}},]
+      }
     ]
   },
 
   plugins: [
-    new MiniCssExtractPlugin({ 
-      filename: 'style.[contenthash].css',
-    }),
+
+    new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
 
     new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: require('cssnano'),
-        cssProcessorPluginOptions: {
-                preset: ['default'],
-        },
-        canPrint: true
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {preset: ['default']},
+      canPrint: true
     }),
 
     new HtmlWebpackPlugin({
@@ -70,7 +52,7 @@ module.exports = {
     new WebpackMd5Hash(),
 
     new webpack.DefinePlugin({
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 
